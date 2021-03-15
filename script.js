305226fedebb80ab7ee16f84341fc5775661e957
Thang Le Quang <lethang7794@gmail.com>
2021-03-16 00:34:44 +0700

    Remove unworking resume button

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
+// ===================== STOPWATCH - BEGIN =================================
+// Convert time to a format of hours, minutes, seconds, and milliseconds
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - ss) * 100;
+  let ms = Math.floor(diffInMs);
+
+  let formattedMM = mm.toString().padStart(2, '0');
+  let formattedSS = ss.toString().padStart(2, '0');
+  let formattedMS = ms.toString().padStart(2, '0');
+
+  return `${formattedMM}:${formattedSS}:${formattedMS}`;
+}
+
+// Declare variables to use in our functions below
+
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+// Create function to modify innerHTML
+
+function print(txt) {
+  document.getElementById('display').innerHTML = txt;
+}
+
+// Create "start", "pause" and "reset" functions
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+  // showButton('PAUSE');
+}
+// ======================== STOPWATCH - END =================================
+
 // Recorder's elements
 let audioElement = document.getElementById('audio');
 let startButton = document.getElementById('start-button');
-let resumeButton = document.getElementById('resume-button');
-let pauseButton = document.getElementById('pause-button');
+let stopButton = document.getElementById('stop-button');
+let timeDisplay = document.getElementById('display');
 
 // Save all the recordings
-let chunks = [];
+let recordedChunks = [];
 
 let mediaRecorder = null;
 
@@ -17,11 +63,10 @@ const getMedia = async () => {
   mediaRecorder = new MediaRecorder(mediaStream);
 
   // Handle buttons clicked after get the media stream
-  resumeButton.onclick = resumeRecording;
-  pauseButton.onclick = pauseRecording;
+  stopButton.onclick = stopRecording;
 
   // Handle Blob data after it being made available for use
-  mediaRecorder.ondataavailable = saveCurrentRecording;
+  mediaRecorder.ondataavailable = handleDataAvailable;
 
   // Create audio file
   mediaRecorder.onstop = sendToMediaPlayer;
@@ -31,51 +76,44 @@ const getMedia = async () => {
 const startRecording = async () => {
   await getMedia();
 
+  recordedChunks = [];
   mediaRecorder.start();
+  start(); // Start stopwatch
 
-  startButton.disabled = false;
+  startButton.disabled = true;
   startButton.style.display = 'none';
 
-  resumeButton.disabled = true;
-  resumeButton.style.display = 'none';
+  stopButton.disabled = false;
+  stopButton.style.display = 'block';
 
-  pauseButton.disabled = false;
-  pauseButton.style.display = 'block';
-};
-
-const resumeRecording = () => {
-  mediaRecorder.start();
-
-  resumeButton.disabled = true;
-  resumeButton.style.display = 'none';
-
-  pauseButton.disabled = false;
-  pauseButton.style.display = 'block';
-
-  audioElement.pause();
+  timeDisplay.style.display = 'block';
   audioElement.style.display = 'none';
 };
 
-// Handle stop button clicked
-const pauseRecording = () => {
+// Handle pause button clicked
+const stopRecording = () => {
   mediaRecorder.stop();
+  clearInterval(timerInterval);
+  print('00:00:00');
+  elapsedTime = 0;
 
-  resumeButton.disabled = false;
-  resumeButton.style.display = 'block';
+  startButton.disabled = false;
+  startButton.style.display = 'block';
 
-  pauseButton.disabled = true;
-  pauseButton.style.display = 'none';
+  stopButton.disabled = true;
+  stopButton.style.display = 'none';
 
+  timeDisplay.style.display = 'none';
   audioElement.style.display = 'block';
 };
 
-const saveCurrentRecording = (e) => {
-  chunks.push(e.data);
-  console.log(chunks);
+const handleDataAvailable = (e) => {
+  recordedChunks.push(e.data);
+  console.log(recordedChunks);
 };
 
 const sendToMediaPlayer = () => {
-  const blob = new Blob(chunks, {
+  const blob = new Blob(recordedChunks, {
     type: 'audio/mp4; codecs=opus',
   });
   const audioURL = URL.createObjectURL(blob);
